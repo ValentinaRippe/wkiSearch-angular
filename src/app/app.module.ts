e22b32a70577da4a32722abba019536fdb43fd30
ValentinaRippe <lauravalentina200102@gmail.com>
2022-05-17 08:07:57 -0500

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchModule } from './pages/search/search.module';
+import { HttpClientModule } from '@angular/common/http'
+import { ArticleModule } from './pages/article/article.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    SearchModule,
+    HttpClientModule,
+    ArticleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/search/services/search.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, pluck } from "rxjs";
+import { environment } from "src/environments/environment";
+
+interface wikipediaResponse{
+    query:{
+        search: Article[]
+    }
+}
+
+export interface Article{
+    ns: number;
+    title: string;
+    pageid: number;
+    size: number;
+    wordcount: number;
+    snippet: string;
+    timestamp: Date;
+}
+
+@Injectable({providedIn: 'root'})
+export class searchServices {
+    constructor(private readonly http: HttpClient){}
+
+    search(term : string):Observable<Article[]>{
+        const params = {
+            action: 'query',
+            format: 'json',
+            list: 'search',
+            srsearch: term,
+            urf8:'1',
+            origin:'*'
+        }
+        //action=query&format=json&list=search&meta=&srsearch=Angular
+            return this.http.get<wikipediaResponse>(environment.api, { params })
+            .pipe(
+                pluck('query', 'search')
+            );
+    } 
+}
